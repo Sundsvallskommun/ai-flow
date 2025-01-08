78efd348055e2356970a42f7f445bdba69a24b11
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2025-01-08 16:32:20 +0100

    WIP

--- src/integration-test/java/apptest/SessionResourceIT.java
+++ src/integration-test/java/apptest/SessionResourceIT.java
@@ -6,17 +6,10 @@ import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import se.sundsvall.ai.flow.Application;
-import se.sundsvall.ai.flow.api.model.Input;
 import se.sundsvall.ai.flow.model.Session;
 import se.sundsvall.ai.flow.service.SessionService;
 import se.sundsvall.dept44.test.AbstractAppTest;
@@ -30,30 +23,20 @@ class SessionResourceIT extends AbstractAppTest {
 	private static final String RESPONSE_FILE = "response.json";
 	private static final String REQUEST_FILE = "request.json";
 
-	private UUID sessionId;
+	private Session session;
 
 	@Autowired
 	private SessionService sessionService;
 
-	@Autowired
-	private WebTestClient webTestClient;
-
 	@BeforeEach
 	void setup() {
-		sessionService.createSession("tjansteskrivelse");
-		var sessions = (Map<UUID, Session>) ReflectionTestUtils.getField(sessionService, "sessions");
-		sessionId = sessions.keySet().stream().findFirst().orElseThrow();
-	}
-
-	@AfterEach
-	void cleanup() {
-		ReflectionTestUtils.setField(sessionService, "sessions", new ConcurrentHashMap<>());
+		session = sessionService.createSession("tjansteskrivelse");
 	}
 
 	@Test
 	void test1_getSession() {
 		setupCall()
-			.withServicePath(PATH + "/" + sessionId)
+			.withServicePath(PATH + "/" + session.getId())
 			.withHttpMethod(GET)
 			.withExpectedResponseStatus(OK)
 			.withExpectedResponse(RESPONSE_FILE)
@@ -74,7 +57,7 @@ class SessionResourceIT extends AbstractAppTest {
 	@Test
 	void test3_addInput() {
 		setupCall()
-			.withServicePath(PATH + "/" + sessionId)
+			.withServicePath(PATH + "/" + session.getId())
 			.withRequest(REQUEST_FILE)
 			.withHttpMethod(POST)
 			.withExpectedResponseStatus(OK)
@@ -84,17 +67,11 @@ class SessionResourceIT extends AbstractAppTest {
 
 	@Test
 	void test4_replaceInput() {
-		// Adds an input to the session.
-		setupCall()
-			.withServicePath(PATH + "/" + sessionId)
-			.withRequest(REQUEST_FILE)
-			.withHttpMethod(POST)
-			.withExpectedResponseStatus(OK)
-			.sendRequestAndVerifyResponse();
+		session.addInput("arendenummer", "VGhpcyBpcyBhbiBvcmlnaW5hbCBhcmVuZGVudW1tZXIgdmFsdWU=");
 
 		// Replaces the input in the session.
 		setupCall()
-			.withServicePath(PATH + "/" + sessionId)
+			.withServicePath(PATH + "/" + session.getId())
 			.withRequest(REQUEST_FILE)
 			.withHttpMethod(PUT)
 			.withExpectedResponseStatus(OK)
@@ -104,26 +81,14 @@ class SessionResourceIT extends AbstractAppTest {
 
 	@Test
 	void test5_runStep() {
-		// Set the required input for the step.
-		webTestClient.post().uri(PATH + "/" + sessionId)
-			.bodyValue(new Input("uppdraget-till-tjansten", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ=="))
-			.exchange()
-			.expectStatus().isOk();
-
-		webTestClient.post().uri(PATH + "/" + sessionId)
-			.bodyValue(new Input("forvaltningens-input", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ=="))
-			.exchange()
-			.expectStatus().isOk();
-
-		webTestClient.post().uri(PATH + "/" + sessionId)
-			.bodyValue(new Input("bakgrundsmaterial",
-				"JVBERi0xLjEKJcOiw6MKMSAwIG9iago8PC9UeXBlL0NhdGFsb2cvUGFnZXM8PC9UeXBlL1BhZ2VzL0NvdW50IDEvS2lkc1syIDAgUl0+Pj4+CmVuZG9iagoyIDAgb2JqCjw8L1R5cGUvUGFnZS9QYXJlbnQgMSAwIFIvTWVkaWFCb3hbMCAwIDU5NCA3OTJdL1Jlc291cmNlczw8L0ZvbnQ8PC9GMSAzIDAgUj4+L1Byb2NTZXRbL1BERi9UZXh0XT4+L0NvbnRlbnRzIDQgMCBSPj4KZW5kb2JqCjMgMCBvYmoKPDwvVHlwZS9Gb250L1N1YnR5cGUvVHlwZTEvTmFtZS9GMS9CYXNlRm9udC9IZWx2ZXRpY2E+PgplbmRvYmoKNCAwIG9iago8PC9MZW5ndGggNSAwIFI+PgpzdHJlYW0KQlQKL0YxIDM2IFRmCjEgMCAwIDEgMjU1IDc1MiBUbQo0OCBUTAooIEhlbGxvKScKKFdvcmxkISknCkVUCmVuZHN0cmVhbQplbmRvYmoKNSAwIG9iago3OAplbmRvYmoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNiBmCjAwMDAwMDAwMTcgMDAwMDAgbgowMDAwMDAwMDk0IDAwMDAwIG4KMDAwMDAwMDIyOCAwMDAwMCBuCjAwMDAwMDAzMDIgMDAwMDAgbgowMDAwMDAwNDI1IDAwMDAwIG4KdHJhaWxlcgo8PC9TaXplIDYvSW5mbyA8PC9DcmVhdGlvbkRhdGUoRDoyMDIzKS9Qcm9kdWNlcihjbWQycGRmKS9UaXRsZShtaW5pLnBkZik+Pi9Sb290IDEgMCBSPj4Kc3RhcnR4cmVmCjQ0NgolJUVPRgoK"))
-			.exchange()
-			.expectStatus().isOk();
+		session.addInput("uppdraget-till-tjansten", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ==");
+		session.addInput("forvaltningens-input", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ==");
+		session.addInput("bakgrundsmaterial",
+			"JVBERi0xLjEKJcOiw6MKMSAwIG9iago8PC9UeXBlL0NhdGFsb2cvUGFnZXM8PC9UeXBlL1BhZ2VzL0NvdW50IDEvS2lkc1syIDAgUl0+Pj4+CmVuZG9iagoyIDAgb2JqCjw8L1R5cGUvUGFnZS9QYXJlbnQgMSAwIFIvTWVkaWFCb3hbMCAwIDU5NCA3OTJdL1Jlc291cmNlczw8L0ZvbnQ8PC9GMSAzIDAgUj4+L1Byb2NTZXRbL1BERi9UZXh0XT4+L0NvbnRlbnRzIDQgMCBSPj4KZW5kb2JqCjMgMCBvYmoKPDwvVHlwZS9Gb250L1N1YnR5cGUvVHlwZTEvTmFtZS9GMS9CYXNlRm9udC9IZWx2ZXRpY2E+PgplbmRvYmoKNCAwIG9iago8PC9MZW5ndGggNSAwIFI+PgpzdHJlYW0KQlQKL0YxIDM2IFRmCjEgMCAwIDEgMjU1IDc1MiBUbQo0OCBUTAooIEhlbGxvKScKKFdvcmxkISknCkVUCmVuZHN0cmVhbQplbmRvYmoKNSAwIG9iago3OAplbmRvYmoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNiBmCjAwMDAwMDAwMTcgMDAwMDAgbgowMDAwMDAwMDk0IDAwMDAwIG4KMDAwMDAwMDIyOCAwMDAwMCBuCjAwMDAwMDAzMDIgMDAwMDAgbgowMDAwMDAwNDI1IDAwMDAwIG4KdHJhaWxlcgo8PC9TaXplIDYvSW5mbyA8PC9DcmVhdGlvbkRhdGUoRDoyMDIzKS9Qcm9kdWNlcihjbWQycGRmKS9UaXRsZShtaW5pLnBkZik+Pi9Sb290IDEgMCBSPj4Kc3RhcnR4cmVmCjQ0NgolJUVPRgoK");
 
 		// Runs the step.
 		setupCall()
-			.withServicePath(PATH + "/" + sessionId + "/arendet")
+			.withServicePath(PATH + "/" + session.getId() + "/arendet")
 			.withHttpMethod(POST)
 			.withExpectedResponseStatus(CREATED)
 			.withExpectedResponse(RESPONSE_FILE)
@@ -132,31 +97,15 @@ class SessionResourceIT extends AbstractAppTest {
 
 	@Test
 	void test6_getStepExecution() {
-		// Set the required input for the step.
-		webTestClient.post().uri(PATH + "/" + sessionId)
-			.bodyValue(new Input("uppdraget-till-tjansten", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ=="))
-			.exchange()
-			.expectStatus().isOk();
+		session.addInput("uppdraget-till-tjansten", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ==");
+		session.addInput("forvaltningens-input", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ==");
+		session.addInput("bakgrundsmaterial",
+			"JVBERi0xLjEKJcOiw6MKMSAwIG9iago8PC9UeXBlL0NhdGFsb2cvUGFnZXM8PC9UeXBlL1BhZ2VzL0NvdW50IDEvS2lkc1syIDAgUl0+Pj4+CmVuZG9iagoyIDAgb2JqCjw8L1R5cGUvUGFnZS9QYXJlbnQgMSAwIFIvTWVkaWFCb3hbMCAwIDU5NCA3OTJdL1Jlc291cmNlczw8L0ZvbnQ8PC9GMSAzIDAgUj4+L1Byb2NTZXRbL1BERi9UZXh0XT4+L0NvbnRlbnRzIDQgMCBSPj4KZW5kb2JqCjMgMCBvYmoKPDwvVHlwZS9Gb250L1N1YnR5cGUvVHlwZTEvTmFtZS9GMS9CYXNlRm9udC9IZWx2ZXRpY2E+PgplbmRvYmoKNCAwIG9iago8PC9MZW5ndGggNSAwIFI+PgpzdHJlYW0KQlQKL0YxIDM2IFRmCjEgMCAwIDEgMjU1IDc1MiBUbQo0OCBUTAooIEhlbGxvKScKKFdvcmxkISknCkVUCmVuZHN0cmVhbQplbmRvYmoKNSAwIG9iago3OAplbmRvYmoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNiBmCjAwMDAwMDAwMTcgMDAwMDAgbgowMDAwMDAwMDk0IDAwMDAwIG4KMDAwMDAwMDIyOCAwMDAwMCBuCjAwMDAwMDAzMDIgMDAwMDAgbgowMDAwMDAwNDI1IDAwMDAwIG4KdHJhaWxlcgo8PC9TaXplIDYvSW5mbyA8PC9DcmVhdGlvbkRhdGUoRDoyMDIzKS9Qcm9kdWNlcihjbWQycGRmKS9UaXRsZShtaW5pLnBkZik+Pi9Sb290IDEgMCBSPj4Kc3RhcnR4cmVmCjQ0NgolJUVPRgoK");
 
-		webTestClient.post().uri(PATH + "/" + sessionId)
-			.bodyValue(new Input("forvaltningens-input", "VGhpcyBpcyBhIGFyZW5kZW51bW1lciB2YWx1ZQ=="))
-			.exchange()
-			.expectStatus().isOk();
-
-		webTestClient.post().uri(PATH + "/" + sessionId)
-			.bodyValue(new Input("bakgrundsmaterial",
-				"JVBERi0xLjEKJcOiw6MKMSAwIG9iago8PC9UeXBlL0NhdGFsb2cvUGFnZXM8PC9UeXBlL1BhZ2VzL0NvdW50IDEvS2lkc1syIDAgUl0+Pj4+CmVuZG9iagoyIDAgb2JqCjw8L1R5cGUvUGFnZS9QYXJlbnQgMSAwIFIvTWVkaWFCb3hbMCAwIDU5NCA3OTJdL1Jlc291cmNlczw8L0ZvbnQ8PC9GMSAzIDAgUj4+L1Byb2NTZXRbL1BERi9UZXh0XT4+L0NvbnRlbnRzIDQgMCBSPj4KZW5kb2JqCjMgMCBvYmoKPDwvVHlwZS9Gb250L1N1YnR5cGUvVHlwZTEvTmFtZS9GMS9CYXNlRm9udC9IZWx2ZXRpY2E+PgplbmRvYmoKNCAwIG9iago8PC9MZW5ndGggNSAwIFI+PgpzdHJlYW0KQlQKL0YxIDM2IFRmCjEgMCAwIDEgMjU1IDc1MiBUbQo0OCBUTAooIEhlbGxvKScKKFdvcmxkISknCkVUCmVuZHN0cmVhbQplbmRvYmoKNSAwIG9iago3OAplbmRvYmoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNiBmCjAwMDAwMDAwMTcgMDAwMDAgbgowMDAwMDAwMDk0IDAwMDAwIG4KMDAwMDAwMDIyOCAwMDAwMCBuCjAwMDAwMDAzMDIgMDAwMDAgbgowMDAwMDAwNDI1IDAwMDAwIG4KdHJhaWxlcgo8PC9TaXplIDYvSW5mbyA8PC9DcmVhdGlvbkRhdGUoRDoyMDIzKS9Qcm9kdWNlcihjbWQycGRmKS9UaXRsZShtaW5pLnBkZik+Pi9Sb290IDEgMCBSPj4Kc3RhcnR4cmVmCjQ0NgolJUVPRgoK"))
-			.exchange()
-			.expectStatus().isOk();
-
-		// Runs the step, which creates a step execution.
-		webTestClient.post().uri(PATH + "/" + sessionId + "/arendet")
-			.exchange()
-			.expectStatus().isCreated();
 
 		// Retrieves the step execution.
 		setupCall()
-			.withServicePath(PATH + "/" + sessionId + "/arendet")
+			.withServicePath(PATH + "/" + session.getId() + "/arendet")
 			.withHttpMethod(GET)
 			.withExpectedResponseStatus(OK)
 			.withExpectedResponse(RESPONSE_FILE)
@@ -166,7 +115,7 @@ class SessionResourceIT extends AbstractAppTest {
 	@Test
 	void test7_generateSessionOutput() {
 		setupCall()
-			.withServicePath(PATH + "/" + sessionId + "/generate")
+			.withServicePath(PATH + "/" + session.getId() + "/generate")
 			.withRequest(REQUEST_FILE)
 			.withHttpMethod(POST)
 			.withExpectedResponseStatus(OK)
